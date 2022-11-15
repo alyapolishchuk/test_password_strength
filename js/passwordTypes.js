8b9855aef8f4b2eb77eeef1190d8c6e73bd24302
Alina Polishchuk <alinochkarul081@gmail.com>
2022-11-15 12:29:21 +0000

    c

---
+++ js/passwordTypes.js
@@ -0,0 +1,14 @@
+const letter = "qwertyuiopasdfghjklzxcvbnm";
+const capital = "QWERTYUIOPASDFGHJKLZXCVBNM";
+const symbol = "§±!@#$%^&*:;|/?.,`~";
+const number = "1234567890";
+
+const password = document.getElementById("userPassword");
+const easyPassword = document.getElementById("easy");
+const averagePassword = document.getElementById("average");
+const strongPassword = document.getElementById("strong");
+
+password.addEventListener('keyup', function (event) {
+    let passwordElement = password.value; // getting the value of the entered element in the field
+
+})
